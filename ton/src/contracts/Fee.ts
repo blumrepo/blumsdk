22b4f4f8b1a323f659516b4251354c0a59174118
EA <ealymbaev@gmail.com>
2025-02-04 14:47:28 +0600

    Integrate new Wallet contract

--- ton/src/contracts/Fee.ts
+++ ton/src/contracts/Fee.ts
@@ -4,8 +4,8 @@ export abstract class Fee {
   static deployGas = toNano(0.02)
   static initialGas = toNano(0.015)
   static buyGas = toNano(0.08)
-  static sellGas = toNano(0.02)
-  static burnGas = toNano(0.05)
+  static sellNotificationGas = toNano(0.02)
+  static sellGas = toNano(0.05)
 
   static buyForward = toNano(0.04)
 }
